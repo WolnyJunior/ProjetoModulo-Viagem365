7b8c5858748e8c4b4e9aa382a562cee3d6c17b4e
WolnyJunior <soares.junior.dev@gmail.com>
2024-09-20 19:11:08 -0300

    :construction: Construção das Rotas

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,17 @@
-import { LogOut, User } from 'lucide-react'
+import { User } from 'lucide-react'
 import { useAuth } from '../../contexts/auth/auth'
 import styles from './styles.module.css'
 import Navbar from '../navbar/navbar'
 
 function Header() {
-    const { user, signOut } = useAuth()
+    const { user } = useAuth()
 
     return (
         <>
-            <div className={`${styles.navbar} bg-dark-subtle`}>
+            <div className={`${styles.header} bg-dark-subtle`}>
                 <h4 className='text-secondary'>Viagem365</h4>
                 <div className={styles.icons}>
                     <span className="text-secondary"><User /></span>
-                    <button className="btn btn-secondary"><LogOut /></button>
                 </div>
             </div>
                 <Navbar></Navbar>
